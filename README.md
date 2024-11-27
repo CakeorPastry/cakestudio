0e7934c3d6ca02626a871d534ab5d78e52a17c9a
CakeorPastry <5daycheiffree@gmail.com>
2024-11-27 12:41:42 +0530

    Clean up files to start backend branch

--- README.md
+++
@@ -1,2 +0,0 @@
-# cakestudio
-A website made for the funny testing lol. This was made for learning basic coding and has only a few projects.
--- script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const toggleButton = document.getElementById('toggleDarkModeButton');
-
-    toggleButton.addEventListener('click', function() {
-        document.body.classList.toggle('dark-mode');
-        this.textContent = document.body.classList.contains('dark-mode') ? '☀️' : '🌙';
-    });
-});

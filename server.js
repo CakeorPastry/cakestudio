a329ac1b4a71ddd97dea70c6a584bccc808cc503
CakeorPastry <91412894+CakeorPastry@users.noreply.github.com>
2024-11-27 13:18:32 +0530

    server.js

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 const allowedOrigins = process.env.ALLOWED_ORIGINS ? process.env.ALLOWED_ORIGINS.split(',') : [];
 
 function restrictedCors(req, res, next) {

0e27c229e4cdd8919143a8cf2f61fbfc1bac8153
CakeorPastry <91412894+CakeorPastry@users.noreply.github.com>
2024-12-30 12:27:44 +0530

    Update auth.js

--- Javascript/auth.js
+++ Javascript/auth.js
@@ -1,1 +1,38 @@
-//
+const urlParams = new URLSearchParams(window.location.search);
+const tokenParam = urlParams.get("token");
+const redirectParam = urlParams.get("redirect") || "/";
+
+async function callback() {
+    if (tokenParam) {
+        try {
+            const response = await fetch(`https://cakestudio.onrender.com/api/auth/validatetoken?token=${encodeURIComponent(tokenParam)}`);
+            const data = await response.json();
+            
+            if (!response.ok) {
+                throw {
+                    status: response.status, 
+                    message: "Invalid login or tampering detected."
+                };
+            }
+            localStorage.setItem("jwtToken", tokenParam);
+        }
+        catch (err) {
+            const errMsg = `Failed to login\nHTTP Status Code : ${err.status}\nError Message : ${err.message}`
+            console.error(errMsg);
+            alert(errMsg);
+        }
+        finally {
+            redirect(); 
+        }
+    }
+    else {
+        redirect();
+    }
+};
+
+function redirect() {
+    const redirectUrl = `https://cakeorpastry.netlify.app${redirectParam}`;
+    window.location.href = redirectUrl;
+};
+
+callback();

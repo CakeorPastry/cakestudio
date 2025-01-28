ef4878078e87b5743010f5d1465155ebc8f366a5
CakeorPastry <91412894+CakeorPastry@users.noreply.github.com>
2025-01-28 08:57:15 +0530

    Update LocalScript.lua
    Dragging GUI fixed..?

--- imsorry/pleaseforgiveme/LocalScript.lua
+++ imsorry/pleaseforgiveme/LocalScript.lua
@@ -164,11 +164,10 @@ TextBox.FocusLost:Connect(function(enterPressed)
 end)
 
 -- 🎭 GUI Dragging Fix
-local dragging = false
-local dragInput, dragStart, startPos
+local dragging, dragInput, dragStart, startPos = false, nil, nil, nil
 
 local function update(input)
-    if dragging then -- Ensures Update Only When Dragging
+    if dragging then
         local delta = input.Position - dragStart
         Frame.Position = UDim2.new(
             startPos.X.Scale, startPos.X.Offset + delta.X,
@@ -178,11 +177,10 @@ local function update(input)
 end
 
 Frame.InputBegan:Connect(function(input)
-    if input.UserInputType == Enum.UserInputType.MouseButton1 then
+    if input.UserInputType == Enum.UserInputType.MouseButton1 or input.UserInputType == Enum.UserInputType.Touch then
         dragging = true
         dragStart = input.Position
         startPos = Frame.Position
-
         input.Changed:Connect(function()
             if input.UserInputState == Enum.UserInputState.End then
                 dragging = false
@@ -192,13 +190,13 @@ Frame.InputBegan:Connect(function(input)
 end)
 
 Frame.InputChanged:Connect(function(input)
-    if input.UserInputType == Enum.UserInputType.MouseMovement then
+    if input.UserInputType == Enum.UserInputType.MouseMovement or input.UserInputType == Enum.UserInputType.Touch then
         dragInput = input
     end
 end)
 
 UserInputService.InputChanged:Connect(function(input)
-    if input == dragInput then
+    if input == dragInput and dragging then
         update(input)
     end
 end)

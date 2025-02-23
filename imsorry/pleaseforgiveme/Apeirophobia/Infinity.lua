8de14f8fa41a9c1c22476c9458e99fbd7c2a6198
CakeorPastry <91412894+CakeorPastry@users.noreply.github.com>
2025-02-23 19:34:23 +0530

    Update Infinity.lua

--- imsorry/pleaseforgiveme/Apeirophobia/Infinity.lua
+++ imsorry/pleaseforgiveme/Apeirophobia/Infinity.lua
@@ -1,3 +1,11 @@
 local Players = game:GetService("Players") 
 
 local Player = Players.LocalPlayer
+local Character = Player.Character or Player.CharacterAdded:Wait() 
+local HumanoidRootPart = Character:WaitForChild("HumanoidRootPart")
+local Humanoid = Character:WaitForChild("Humanoid") 
+
+local exits = {
+    [0] = Vector3.new(-902.1170654296875, 11.285065650939941, -92.56808471679688), 
+    [1] = 
+}

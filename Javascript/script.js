3ada3d103896c118464c5f4dd1ee8cbf4b5ef7a0
CakeorPastry <91412894+CakeorPastry@users.noreply.github.com>
2025-03-19 15:00:31 +0530

    Update script.js

--- Javascript/script.js
+++ Javascript/script.js
@@ -5,4 +5,18 @@ document.addEventListener('DOMContentLoaded', function() {
         document.body.classList.toggle('dark-mode');
         this.textContent = document.body.classList.contains('dark-mode') ? '☀️' : '🌙';
     });
+
+    function minorErrorMessageHandler(message) {
+    const minorErrorMessage = document.getElementById("minorErrorMessage");
+    
+    if (!minorErrorMessage) { return; };
+
+    minorErrorMessage.innerText = message;
+    minorErrorMessage.parentNode.parentNode.style.display = "flex"
+    sendButton.disabled = true;
+    setTimeout(() => {
+        sendButton.disabled = false;
+        minorErrorMessage.parentNode.parentNode.style.display = "none";
+    }, 3000)
+}
 });

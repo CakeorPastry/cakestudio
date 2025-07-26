fd93240eb3aadeaad23f96cd2fcfb6ee913a46ed
CakeorPastry <91412894+CakeorPastry@users.noreply.github.com>
2025-07-26 18:14:59 +0530

    Create unxpctd-a1.lua

---
+++ Scripts/unxpctd-a1.lua
@@ -0,0 +1,5 @@
+--[[
+This is a sample unindented Luau code
+]]
+
+local part = Instance.new("Part");part.Name = "MyPart";part.Size = Vector3.new(4,1,2);part.Position = Vector3.new(0,5,0);part.Anchored = true;part.BrickColor = BrickColor.new("Bright red");part.Parent = workspace;

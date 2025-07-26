a3d31d221b0b6772aacec20f92df949a3b278cc1
CakeorPastry <91412894+CakeorPastry@users.noreply.github.com>
2025-07-26 18:27:27 +0530

    Update unxpctd-a1.lua

--- Scripts/unxpctd-a1.lua
+++ Scripts/unxpctd-a1.lua
@@ -2,4 +2,4 @@
 This is a sample unindented Luau code
 ]]
 
-local part = Instance.new("Part");part.Name = "MyPart";part.Size = Vector3.new(4,1,2);part.Position = Vector3.new(0,5,0);part.Anchored = true;part.BrickColor = BrickColor.new("Bright red");part.Parent = workspace;
+local part = Instance.new("Part");part.Name = "MyPart";part.Size = Vector3.new(4,1,2);part.Position = Vector3.new(0,5,0);part.Anchored = true;part.BrickColor = BrickColor.new("Bright red");part.Parent = workspace;localtable={"n", 1,5};;;;print(" done bro ");

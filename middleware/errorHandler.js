fc47cf0dffa68ddba7646afeaf036939fe440976
CakeorPastry <91412894+CakeorPastry@users.noreply.github.com>
2025-10-01 12:13:14 +0530

    Update errorHandler.js

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,29 +1,28 @@
 const requestError = require('./auxiliary/requestError');
 
-// 404 handler
+// 404 Handler
 function notFoundHandler(req, res, next) {
-
-  requestError({
-    req,
-    res,
-    errorCode: 404,
-  });
+  requestError({
+    req,
+    res,
+    errorCode: 404
+  });
 }
 
 // General error handler middleware (500)
 function errorHandler(err, req, res, next) {
-  console.error(err.stack);
+  console.error(err.stack);
+  const statusCode = Number(err.status || err.statusCode) || 500;
 
-  const statusCode = 500; /* (err.status || err.statusCode || 500;) */
-
-  requestError({
-    req,
-    res,
-    errorCode: statusCode,
-  });
+  requestError({
+    req,
+    res,
+    errorCode: statusCode,
+    debugMessage: err.message
+  });
 }
 
 module.exports = {
-  notFoundHandler,
-  errorHandler,
+  notFoundHandler,
+  errorHandler
 };
